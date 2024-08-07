91da481df19670b9439392841405ed413a3c82d4
Swapnil Gavali <146269131+Swapnil-CS775@users.noreply.github.com>
2024-08-07 21:45:50 +0530

    Update day11_7-8.java

--- day11_7-8.java
+++ day11_7-8.java
@@ -1,22 +1,18 @@
 /*
- * 704. Binary Search
+704. Binary Search
 Easy
 Runtime - 0 ms | Beats 100%
 Memory - 45.80 MB | Beats 28.04%
 
 Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
-
 You must write an algorithm with O(log n) runtime complexity.
 
- 
-
 Example 1:
-
 Input: nums = [-1,0,3,5,9,12], target = 9
 Output: 4
 Explanation: 9 exists in nums and its index is 4
-Example 2:
 
+Example 2:
 Input: nums = [-1,0,3,5,9,12], target = 2
 Output: -1
 Explanation: 2 does not exist in nums so return -1
