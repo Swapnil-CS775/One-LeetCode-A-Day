feadec0c2e4b4d76cb5e385a2baac009eaddf6bf
Swapnil Gavali <146269131+Swapnil-CS775@users.noreply.github.com>
2024-09-13 23:57:01 +0530

    Update 8_1029. Two City Scheduling.java

--- 8_1029. Two City Scheduling.java
+++ 8_1029. Two City Scheduling.java
@@ -1,10 +1,10 @@
 /*
 1029. Two City Scheduling
 Medium
-Topics
-Companies
-A company is planning to interview 2n people. Given the array costs where costs[i] = [aCosti, bCosti], the cost of flying the ith person to city a is aCosti, and the cost of flying the ith person to city b is bCosti.
+Runtime - 2 ms | Beats - 82.47%
+Memory - 41.88 MB | Beats - 28.23%
 
+A company is planning to interview 2n people. Given the array costs where costs[i] = [aCosti, bCosti], the cost of flying the ith person to city a is aCosti, and the cost of flying the ith person to city b is bCosti.
 Return the minimum cost to fly every person to a city such that exactly n people arrive in each city.
 
  
