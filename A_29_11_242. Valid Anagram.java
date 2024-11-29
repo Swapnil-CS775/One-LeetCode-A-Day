c0bf60a0fb2d1bb049d30cd083b9b9153917214c
Swapnil Gavali <146269131+Swapnil-CS775@users.noreply.github.com>
2024-11-29 17:33:59 +0530

    Update A_29_11_242. Valid Anagram.java

--- A_29_11_242. Valid Anagram.java
+++ A_29_11_242. Valid Anagram.java
@@ -56,3 +56,56 @@ class Solution {
         return true;
     }
 }
+
+// another small change
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] charCount = new int[26]; 
+
+        for (int i = 0; i < s.length(); i++) {
+            charCount[s.charAt(i) - 'a']++;
+        }
+        for (int j = 0; j < t.length(); j++) {
+            charCount[t.charAt(j) - 'a']--;
+            if(charCount[t.charAt(j) - 'a']<0)
+            {
+                return false;
+            }
+        }
+    return true;
+    }
+}
+
+
+
+// using hash MAp
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character,Integer>myMap=new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            myMap.put(s.charAt(i),myMap.getOrDefault(s.charAt(i),0)+1);
+            myMap.put(t.charAt(i),myMap.getOrDefault(t.charAt(i),0)-1);
+        }
+
+        for (int j : myMap.values()) {
+            if (j != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
