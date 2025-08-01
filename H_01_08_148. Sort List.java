279bfccffa01dda18e2e8534ac0b0bc24948c0cf
Swapnil Gavali <146269131+Swapnil-CS775@users.noreply.github.com>
2025-08-01 15:58:43 +0530

    Create H_01_08_148. Sort List.java

---
+++ H_01_08_148. Sort List.java
@@ -0,0 +1,73 @@
+/*
+148. Sort List
+Attempted
+Medium
+Topics
+premium lock icon
+Companies
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+ 
+
+Example 1:
+
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105
+ 
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+
+
+*/
+
+//code 
+//this is one of the possible ans but it gives TLE for large input
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        
+        ListNode first=head;
+        ListNode second=head;
+
+        while(first!=null){
+            second=head;
+            while(second.next!=null){
+
+                if(second.val>second.next.val){
+                    int temp=second.val;
+                    second.val=second.next.val;
+                    second.next.val=temp;
+                }
+                second=second.next;
+            }
+            first=first.next;
+        }
+        return head;
+    }
+}
+
