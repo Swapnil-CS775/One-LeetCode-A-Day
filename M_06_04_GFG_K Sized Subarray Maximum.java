21d9b2c2254be4754322f4ac8f6d00b50360e62e
Swapnil Gavali <146269131+Swapnil-CS775@users.noreply.github.com>
2026-04-06 12:56:20 +0530

    Create M_06_04_GFG_K Sized Subarray Maximum.java

---
+++ M_06_04_GFG_K Sized Subarray Maximum.java
@@ -0,0 +1,54 @@
+/*
+K Sized Subarray Maximum
+Difficulty: MediumAccuracy: 26.04%Submissions: 440K+Points: 4
+Given an array arr[] of positive integers and an integer k. You have to find the maximum value for each contiguous subarray of size k. Return an array of maximum values corresponding to each contiguous subarray.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 1, 4, 5, 2, 3, 6], k = 3
+Output: [3, 3, 4, 5, 5, 5, 6]
+Explanation: 
+1st contiguous subarray [1, 2, 3], max = 3
+2nd contiguous subarray [2, 3, 1], max = 3
+3rd contiguous subarray [3, 1, 4], max = 4
+4th contiguous subarray [1, 4, 5], max = 5
+5th contiguous subarray [4, 5, 2], max = 5
+6th contiguous subarray [5, 2, 3], max = 5
+7th contiguous subarray [2, 3, 6], max = 6
+Input: arr[] = [5, 1, 3, 4, 2], k = 1
+Output: [5, 1, 3, 4, 2]
+Explanation: When k = 1, each element in the array is its own subarray, so the output is simply the same array
+Constraints:
+1 ≤ arr.size() ≤ 106
+1 ≤ k ≤ arr.size()
+0 ≤ arr[i] ≤ 109
+*/
+
+
+//code -
+//using deque
+
+class Solution {
+    public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        // code here
+        Deque<Integer>deque=new LinkedList<>();
+        ArrayList<Integer>list=new ArrayList<>();
+        int start=0;
+        
+        int i;
+        for(i=0;i<k;i++){
+            while(!deque.isEmpty() && arr[deque.peekLast()]<arr[i]) deque.removeLast();
+            deque.addLast(i);
+        }
+        list.add(arr[deque.peekFirst()]);
+        for(i=k;i<arr.length;i++){
+            start++;
+            if(!deque.isEmpty() && deque.peekFirst()<start) deque.removeFirst();
+            while(!deque.isEmpty() && arr[deque.peekLast()]<arr[i]) deque.removeLast();
+            deque.addLast(i);
+            list.add(arr[deque.peekFirst()]);
+        }
+        
+        return list;
+    }
+}
